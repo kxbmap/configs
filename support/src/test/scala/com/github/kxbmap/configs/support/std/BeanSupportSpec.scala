e18e5e84b102461a6d4f21b50328fa62dab1adb6
Philip L. McMahon <philip.l.mcmahon@gmail.com>
2015-04-18 14:02:05 -0700

    Support primitive types when resolving setter method

--- support/src/test/scala/com/github/kxbmap/configs/support/std/BeanSupportSpec.scala
+++ support/src/test/scala/com/github/kxbmap/configs/support/std/BeanSupportSpec.scala
@@ -17,7 +17,8 @@
 package com.github.kxbmap.configs
 package support.std
 
-import com.typesafe.config.ConfigFactory
+import java.util.{List => JList}
+import com.typesafe.config.{ConfigException, ConfigFactory}
 import org.scalatest.{FunSpec, Matchers}
 
 class BeanSupportSpec extends FunSpec with Matchers with BeanSupport {
@@ -26,17 +27,23 @@ class BeanSupportSpec extends FunSpec with Matchers with BeanSupport {
 
   describe("generic bean support") {
     val c = ConfigFactory.parseString(
-      """a.foo=x
-        |b.bar=1
-        |c.abc=xyz""".stripMargin)
+      """a.string=x
+        |b.int=one
+        |c.missing=prop
+        |d.boolean=true
+        |d.double=0.1
+        |d.int=1
+        |d.list=[foo, bar, baz]
+        |d.long=2
+        |d.string=x""".stripMargin)
 
     implicit val topConfigs = Beans[Top]
     implicit val innerConfigs = Beans { new Inner }
 
     it ("should be available to get a value with a no-arg constructor") {
       val o = c.get[Top]("a")
-      o._foo shouldBe "x"
-      o._bar shouldBe null // Omitted from config
+      o._string shouldBe "x"
+      o._list shouldBe null // Omitted from config
     }
 
     it ("should return different instances with a no-arg constructor") {
@@ -45,8 +52,8 @@ class BeanSupportSpec extends FunSpec with Matchers with BeanSupport {
 
     it ("should be available to get a value via factory function") {
       val o = c.get[Inner]("a")
-      o._foo shouldBe "x"
-      o._bar shouldBe null
+      o._string shouldBe "x"
+      o._list shouldBe null
     }
 
     it ("should return different instances with factory function") {
@@ -54,24 +61,56 @@ class BeanSupportSpec extends FunSpec with Matchers with BeanSupport {
     }
 
     it ("should throw an exception for incorrect property types") {
-      intercept[NoSuchMethodException] { c.get[Inner]("b") }
+      intercept[ConfigException.WrongType] { c.get[Inner]("b") }
     }
 
     it ("should throw an exception for unknown property names") {
-      intercept[NoSuchMethodException] { c.get[Inner]("c") }
+      intercept[ConfigException.BadPath] { c.get[Inner]("c") }
     }
+
+    it ("should support both primitive types and objects") {
+      val o = c.get[Top]("d")
+      o._boolean shouldBe true
+      o._int shouldBe 1
+      o._list should have length 3
+      o._long shouldBe 2
+      o._string shouldBe "x" // Omitted from config
+    }
+
   }
 
 }
 
 trait Obj {
 
-  var _foo: String = _
-  var _bar: String = _
+  var _boolean: Boolean = _
+  var _double: Double = _
+  var _int: Int = _
+  var _list: JList[String] = _
+  var _long: Long = _
+  var _string: String = _
 
-  def setFoo(s: String): Unit = _foo = s
-  def setBar(s: String): Unit = _bar = s
+  def setBoolean(b: Boolean): Unit = _boolean = b
+  def setDouble(d: Double): Unit = _double = d
+  def setInt(i: Int): Unit = _int = i
+  def setList(l: JList[String]): Unit = _list = l
+  def setLong(l: Long): Unit = _long = l
+  def setString(s: String): Unit = _string = s
 
 }
 
 class Top extends Obj
+
+class Primitives {
+
+  var _boolean: Boolean = _
+  var _double: Double = _
+  var _int: Int = _
+  var _long: Long = _
+
+  def setBoolean(b: Boolean): Unit = _boolean = b
+  def setDouble(d: Double): Unit = _double = d
+  def setInt(i: Int): Unit = _int = i
+  def setLong(l: Long): Unit = _long = l
+
+}
