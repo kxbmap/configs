d9067222dd1ebc750c9db26a00334df4924cc281
kxbmap <kxbmap@gmail.com>
2015-05-28 21:29:07 +0900

    Update Copyright

--- ext/scalikejdbc/src/main/scala/com/github/kxbmap/configs/support/scalikejdbc/package.scala
+++ ext/scalikejdbc/src/main/scala/com/github/kxbmap/configs/support/scalikejdbc/package.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 Tsukasa Kitachi
+ * Copyright 2013-2015 Tsukasa Kitachi
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
