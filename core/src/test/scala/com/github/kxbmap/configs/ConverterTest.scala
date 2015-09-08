d5a952eebb58ab3211bcfa82f1dcf3f67c716f1a
kxbmap <kxbmap@gmail.com>
2015-09-08 09:19:48 +0900

    Refactor converter test

--- core/src/test/scala/com/github/kxbmap/configs/ConverterTest.scala
+++ core/src/test/scala/com/github/kxbmap/configs/ConverterTest.scala
@@ -43,54 +43,48 @@ object ConverterTest extends Scalaprops {
   }
 
   val fromString = {
-    def to[A: Converter.FromString : ClassTag](expected: String => A)(implicit G: Gen[String :@ A]) =
-      forAll { (s: String :@ A) =>
-        Converter[String, A].convert(s) == expected(s)
+    def to[A: Converter.FromString : Gen : ClassTag](string: A => String = (_: A).toString) =
+      forAll { (a: A) =>
+        Converter[String, A].convert(string(a)) == a
       }.toProperties(s"to ${classTag[A].runtimeClass.getSimpleName}")
 
     Properties.list(
-      to[Symbol](Symbol.apply),
-      to[JavaEnum](JavaEnum.valueOf),
-      to[UUID](UUID.fromString),
-      to[Locale](s => Locale.getAvailableLocales.find(_.toString == s).get),
-      to[Path](Paths.get(_)),
-      to[File](new File(_)),
-      to[InetAddress](InetAddress.getByName),
-      to[URI](new URI(_))
+      to[Symbol](_.name),
+      to[JavaEnum](),
+      to[UUID](),
+      to[Locale](),
+      to[Path](),
+      to[File](),
+      to[InetAddress](_.getHostAddress),
+      to[URI]()
     )
   }
 
-  implicit lazy val symbolStringGen: Gen[String :@ Symbol] =
-    Gen[String].tag[Symbol]
+  implicit lazy val uuidGen: Gen[UUID] =
+    Gen[Array[Byte]].map(UUID.nameUUIDFromBytes)
 
-  implicit lazy val javaEnumStringGen: Gen[String :@ JavaEnum] =
-    Gen[JavaEnum].map(_.name()).tag[JavaEnum]
-
-  implicit lazy val uuidStringGen: Gen[String :@ UUID] =
-    Gen[Array[Byte]].map(UUID.nameUUIDFromBytes).map(_.toString).tag[UUID]
-
-  implicit lazy val localeStringGen: Gen[String :@ Locale] = {
-    val ls = Locale.getAvailableLocales.map(_.toString)
-    Gen.elements(ls.head, ls.tail: _*).tag[Locale]
+  implicit lazy val localeGen: Gen[Locale] = {
+    val ls = Locale.getAvailableLocales
+    Gen.elements(ls.head, ls.tail: _*)
   }
 
-  implicit lazy val pathStringGen: Gen[String :@ Path] =
-    Gen.nonEmptyList(Gen.nonEmptyString(Gen.alphaChar)).map(_.list.mkString(File.separator)).tag[Path]
+  implicit lazy val pathGen: Gen[Path] =
+    Gen.nonEmptyList(Gen.nonEmptyString(Gen.alphaChar)).map(p => Paths.get(p.head, p.tail: _*))
 
-  implicit lazy val fileStringGen: Gen[String :@ File] =
-    pathStringGen.untag.tag[File]
+  implicit lazy val fileGen: Gen[File] =
+    pathGen.map(_.toFile)
 
-  implicit lazy val inetAddressStringGen: Gen[String :@ InetAddress] = {
+  implicit lazy val inetAddressGen: Gen[InetAddress] = {
     val p = Gen.choose(0, 255)
-    Apply[Gen].apply4(p, p, p, p)((a, b, c, d) => s"$a.$b.$c.$d").tag[InetAddress]
+    Apply[Gen].apply4(p, p, p, p)((a, b, c, d) => s"$a.$b.$c.$d").map(InetAddress.getByName)
   }
 
-  implicit lazy val uriStringGen: Gen[String :@ URI] = {
+  implicit lazy val uriGen: Gen[URI] = {
     val str = Gen.nonEmptyString(Gen.alphaChar)
     val opt = Gen.option(str)
     Apply[Gen].apply3(opt, str, opt) {
-      (scheme, ssp, fragment) => s"${scheme.fold("")(_ + ":")}$ssp${fragment.fold("")("#" + _)}"
-    }.tag[URI]
+      (scheme, ssp, fragment) => new URI(scheme.orNull, ssp, fragment.orNull)
+    }
   }
 
 }
