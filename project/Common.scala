c42bdd5a47b951d3936a75d2675e994be51e6330
kxbmap <kxbmap@gmail.com>
2016-09-11 21:57:25 +0900

    Scala 2.12.0-RC1
    (cherry picked from commit 5698357)

--- project/Common.scala
+++ project/Common.scala
@@ -7,7 +7,7 @@ object Common extends AutoPlugin {
 
   override lazy val projectSettings: Seq[Setting[_]] = Seq(
     scalaVersion := "2.11.8",
-    crossScalaVersions += "2.12.0-M5",
+    crossScalaVersions += "2.12.0-RC1",
     description := "Scala wrapper for Typesafe config",
     organization := "com.github.kxbmap",
     scalacOptions ++= Seq(
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -38,10 +38,10 @@ object Dependencies extends AutoPlugin {
   override lazy val projectSettings: Seq[Setting[_]] = Seq(
     configVersion := "1.3.0",
     lombokVersion := "1.16.10",
-    scalapropsVersion := "0.3.2",
+    scalapropsVersion := "0.3.4",
     java8CompatVersion := (CrossVersion.partialVersion(scalaVersion.value) match {
       case Some((2, 11)) => "0.7.0"
-      case _ => "0.8.0-RC3"
+      case _ => "0.8.0-RC5"
     })
   )
 
