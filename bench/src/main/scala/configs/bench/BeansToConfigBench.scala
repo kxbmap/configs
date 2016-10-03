0f6002072afa291346383f3b7654c730c738d6c0
kxbmap <kxbmap@gmail.com>
2016-10-03 22:09:44 +0900

    Reduce default benchmark iterations

--- bench/src/main/scala/configs/bench/BeansToConfigBench.scala
+++ bench/src/main/scala/configs/bench/BeansToConfigBench.scala
@@ -24,8 +24,8 @@ import scala.util.Random
 
 @Fork(1)
 @Threads(1)
-@Warmup(iterations = 10)
-@Measurement(iterations = 10)
+@Warmup(iterations = 3)
+@Measurement(iterations = 3)
 @State(Scope.Thread)
 class BeansToConfigBench {
 
