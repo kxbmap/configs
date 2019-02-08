21bdb9b9b3aec40e025cde3cfe975c9774258eab
Seth Tisue <seth@tisue.net>
2019-02-08 13:56:03 -0800

    bump Scala 2.13 version (from M4 to M5)

--- project/Common.scala
+++ project/Common.scala
@@ -25,7 +25,7 @@ object Common extends AutoPlugin {
 
   override lazy val projectSettings: Seq[Setting[_]] = Seq(
     scalaVersion := "2.12.8",
-    crossScalaVersions := Seq("2.12.8", "2.11.12", "2.13.0-M4"),
+    crossScalaVersions := Seq("2.12.8", "2.11.12", "2.13.0-M5"),
     scalapropsVersion := "0.5.5",
     scalacOptions ++= Seq(
       "-deprecation",
