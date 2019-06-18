745bcb0dfedc0d0b075b54adf5d96fbdf4012c69
kxbmap <kxbmap@gmail.com>
2019-06-18 20:24:42 +0900

    Set default Scala version to 2.13.0

--- project/Common.scala
+++ project/Common.scala
@@ -24,8 +24,8 @@ object Common extends AutoPlugin {
   }
 
   override lazy val projectSettings: Seq[Setting[_]] = Seq(
-    scalaVersion := "2.12.8",
-    crossScalaVersions := Seq("2.12.8", "2.11.12", "2.13.0"),
+    scalaVersion := "2.13.0",
+    crossScalaVersions := Seq("2.13.0", "2.12.8", "2.11.12"),
     scalapropsVersion := "0.6.0",
     scalacOptions ++= Seq(
       "-deprecation",
