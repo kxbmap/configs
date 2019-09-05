f74cfefe3a4f5c463134824f7ea5d10249af3c4f
kxbmap <kxbmap@gmail.com>
2019-09-06 02:14:27 +0900

    scalaprops 0.6.1

--- project/Common.scala
+++ project/Common.scala
@@ -26,7 +26,7 @@ object Common extends AutoPlugin {
   override lazy val projectSettings: Seq[Setting[_]] = Seq(
     scalaVersion := "2.13.0",
     crossScalaVersions := Seq("2.13.0", "2.12.8", "2.11.12"),
-    scalapropsVersion := "0.6.0",
+    scalapropsVersion := "0.6.1",
     scalacOptions ++= Seq(
       "-deprecation",
       "-unchecked",
